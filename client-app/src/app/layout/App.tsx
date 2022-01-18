4404e26cfe11369a713d90e5eae6edc055751a98
KyrillosAntoon <hunter.steele.1@gmail.com>
2022-01-18 15:21:22 -0600

    -Finished video 64

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,27 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponents';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined >(undefined);
   const [editMode, setEditMode] = useState(false);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(resopnse => {
-      setActivities(resopnse.data);
+    agent.Activities.list().then(resopnse => {
+      let activities: Activity[] = [];
+      resopnse.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      setLoading(false);
     })
   },[]);
 
@@ -50,6 +57,8 @@ function App() {
 {
   setActivities([...activities.filter(x => x.id !== id)])
 }
+
+if(loading) return <LoadingComponent content='Loading app' />
 
   return (
     <Fragment >
