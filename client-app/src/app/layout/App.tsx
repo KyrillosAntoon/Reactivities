d7a6d5e40b77b151eb449deae4515d72ca80aa53
KyrillosAntoon <hunter.steele.1@gmail.com>
2022-02-01 16:56:28 -0600

    -Finished Video 82

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponents';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
-  const {activityStore} = useStore();
 
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
 
 
@@ -21,8 +16,9 @@ function App() {
     <div>
      <NavBar/>
     <Container style={{marginTop:'7em'}}>
-
-   <ActivityDashboard/>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/activities' component={ActivityDashboard} />
+      <Route path='/createActivity' component={ActivityForm} />
     </Container>
     </div>
   );
