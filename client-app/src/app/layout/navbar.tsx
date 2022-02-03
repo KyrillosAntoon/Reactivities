29f76bba8cfd1c016e2d02f7b9d47cf1c312cde7
KyrillosAntoon <hunter.steele.1@gmail.com>
2022-02-03 17:56:15 -0600

    -Finished video 85

--- client-app/src/app/layout/navbar.tsx
+++ client-app/src/app/layout/navbar.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
 
 interface Props{
 }
 
 export default function NavBar(){
-
-    const {activityStore} = useStore();
 
 
     return(
@@ -20,7 +17,7 @@ export default function NavBar(){
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' exact name='Activities' />
                 <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' exact onClick={() => activityStore.openForm()} positive content='Create Activity'/>
+                    <Button as={NavLink} to='/createActivity' exact  positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
