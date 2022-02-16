e14809d4f6b747d11dbd3ff70143d7d67e5c83e2
KyrillosAntoon <hunter.steele.1@gmail.com>
2022-02-16 16:56:42 -0600

    -Finished video 107

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -4,11 +4,13 @@ using Domain;
 using MediatR;
 using Persistence;
 using FluentValidation;
+using Application.Core;
+
 namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
@@ -19,7 +21,7 @@ namespace Application.Activities
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
         }
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -27,13 +29,15 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
+                
+                if(!result) return Result<Unit>.Failure("Failed to create activity");
 
-                return Unit.Value;
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
